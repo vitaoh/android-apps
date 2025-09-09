d0582e1f4a44fc1ce198793fd2d598e2cf81c225
victorherculini <herculinvictorr@gmail.com>
2025-09-09 08:47:12 -0300

    app3 finish

--- App3/app/src/main/java/com/example/app3/MainActivity.kt
+++ App3/app/src/main/java/com/example/app3/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.app3
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private var isUpdating = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -12,6 +16,51 @@ class MainActivity : AppCompatActivity() {
         val etC = findViewById<EditText>(R.id.etC)
         val etF = findViewById<EditText>(R.id.etF)
 
+        etC.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (isUpdating) return
+                isUpdating = true
+                if (!s.isNullOrEmpty()) {
+                    val celsius = s.toString().toDoubleOrNull()
+                    if (celsius != null) {
+                        val fahrenheit = celsius * 9 / 5 + 32
+                        etF.setText(fahrenheit.toString())
+                    } else {
+                        etF.setText("")
+                    }
+                } else {
+                    etF.setText("")
+                }
+                isUpdating = false
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+        etF.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (isUpdating) return
+                isUpdating = true
+                if (!s.isNullOrEmpty()) {
+                    val fahrenheit = s.toString().toDoubleOrNull()
+                    if (fahrenheit != null) {
+                        val celsius = (fahrenheit - 32) * 5 / 9
+                        etC.setText(celsius.toString())
+                    } else {
+                        etC.setText("")
+                    }
+                } else {
+                    etC.setText("")
+                }
+                isUpdating = false
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
     }
 }
+
