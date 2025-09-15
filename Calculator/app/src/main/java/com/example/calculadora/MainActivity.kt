c325b7fa62c1f607962048e35925f10b3c437fc5
victorherculini <herculinvictorr@gmail.com>
2025-09-15 11:21:13 -0300

    start calculator

---
+++ Calculator/app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.calculadora
+
+import android.R
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
